145b0d5cdf3b47a42d0338d6358872d155370bd0
eckrin <eckrin.dev@gmail.com>
2023-09-16 20:50:05 +0900

    230916 test

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,7 @@ import java.util.*;
 
 public class Main
 {
-    static int N;
-    static Set<String> set = new HashSet<>();
-    static int rst = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        for(int i=0; i<N; i++) {
-            String input = br.readLine();
-            if(input.equals("ENTER")) {
-                rst+=set.size();
-                set = new HashSet<>();
-            } else {
-                set.add(input);
-            }
-        }
-        rst+=set.size();
-        System.out.println(rst);
+    public static void main(String[] args) {
+        System.out.println("rest");
     }
 }
