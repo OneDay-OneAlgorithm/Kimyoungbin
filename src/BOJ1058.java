087d297d22d0f48d3cea2203a9ee0deaf7ff2530
eckrin <eckrin.dev@gmail.com>
2023-09-17 15:48:32 +0900

    230917

---
+++ src/BOJ1058.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ1058
+{
+    static int N;
+    static LinkedList<Integer>[] list;
+    static boolean[] visited;
+    static int rst = Integer.MIN_VALUE;
+    static Queue<Node> queue = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        //연결리스트 초기화
+        list = new LinkedList[N+1];
+        for(int i=1; i<=N; i++) {
+            list[i] = new LinkedList<>();
+        }
+        for(int i=1; i<=N; i++) {
+            String[] input = br.readLine().split("");
+            for(int j=0; j<N; j++) {
+                if(input[j].equals("Y"))
+                    list[i].add(j+1);
+            }
+        }
+
+        for(int i=1; i<=N; i++) {
+            visited = new boolean[N+1];
+            rst = Math.max(rst, bfs(i));
+        }
+
+        System.out.println(rst);
+
+    }
+
+    private static int bfs(int start) {
+        int cnt = 0;
+        queue.add(new Node(start, 0));
+        visited[start] = true;
+
+        while(!queue.isEmpty()) {
+            Node poll = queue.poll();
+            for(int i=0; i<list[poll.n].size(); i++) {
+                int next = list[poll.n].get(i);
+                if(!visited[next] && poll.depth<2) {
+                    queue.add(new Node(next, poll.depth+1));
+                    visited[next] = true;
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
+
+class Node {
+    int n;
+    int depth;
+
+    public Node(int n, int depth) {
+        this.n = n;
+        this.depth = depth;
+    }
+}
