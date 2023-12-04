e637b06060501b7df7d94b48a8d744091cb0c39d
eckrin <bsh51515@naver.com>
2023-12-04 22:15:29 +0900

    231204: 2588

---
+++ src/BOJ2588.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2588
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String l1 = br.readLine();
+        String l2 = br.readLine();
+
+        int l3 = Integer.parseInt(l1)*(l2.charAt(2)-'0');
+        int l4 = Integer.parseInt(l1)*(l2.charAt(1)-'0');
+        int l5 = Integer.parseInt(l1)*(l2.charAt(0)-'0');
+        int l6 = l3+l4*10+l5*100;
+
+        System.out.println(l3);
+        System.out.println(l4);
+        System.out.println(l5);
+        System.out.println(l6);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main
 {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws IOException {
+
     }
 }
