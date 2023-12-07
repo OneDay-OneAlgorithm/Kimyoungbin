459e55e8fc2a4abbb8d03540957947db1a0b5e06
eckrin <bsh51515@naver.com>
2023-12-07 23:52:08 +0900

    231207: 18110

--- src/BOJ18110.java
+++ src/BOJ18110.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main
+public class BOJ18110
 {
     static int n, exc, sum = 0;
     static int[] arr;
