7a025082b01525c4e40785e6e9a232680e746d41
eckrin <bsh51515@naver.com>
2023-12-07 23:32:29 +0900

    231207: 15829

--- src/BOJ15829.java
+++ src/BOJ15829.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main
+public class BOJ15829
 {
     static int L, a;
     static String str;
