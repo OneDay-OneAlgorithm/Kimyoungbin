55e525d88fac0aca8a8a4f1ae839742b3449a128
eckrin <bsh51515@naver.com>
2023-12-10 15:17:10 +0900

    231210: 10845

--- src/B_2023_12/BOJ10845.java
+++ src/B_2023_12/BOJ10845.java
@@ -1,10 +1,12 @@
+package B_2023_12;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Deque;
 import java.util.LinkedList;
 
-public class Main
+public class BOJ10845
 {
     static int N;
     static Deque<Integer> queue = new LinkedList<>();
