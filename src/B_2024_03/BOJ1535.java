7eba7dc45ec7ea181e2f13d7200d2c3c910081c6
eckrin <bsh51515@naver.com>
2024-03-06 22:53:37 +0900

    240306: 1535

--- src/B_2024_03/BOJ1535.java
+++ src/B_2024_03/BOJ1535.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+package B_2024_03;
 
-public class Main
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1535
 {
     static int N;
     static int[] L, J; // 피로도, 기쁨
