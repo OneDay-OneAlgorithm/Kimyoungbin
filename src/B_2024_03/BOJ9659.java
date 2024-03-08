ff4ec21ac91eea54f946a9e44df6b9c5672cf0e5
eckrin <bsh51515@naver.com>
2024-03-09 01:33:51 +0900

    240309: 9659

--- src/B_2024_03/BOJ9659.java
+++ src/B_2024_03/BOJ9659.java
@@ -1,13 +1,16 @@
-import java.io.*;
-import java.util.*;
+package B_2024_03;
 
-public class Main
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9659
 {
     static long N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Long.parseLong(br.readLine());
-        System.out.println(N%7==2 || N%7==0 ? "CY" : "SK");
+        System.out.println(N%2==0?"CY":"SK");
     }
 }
 
