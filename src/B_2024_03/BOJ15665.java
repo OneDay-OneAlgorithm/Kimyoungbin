ee223a8d4db21dda63212b47901164c4aee19cc4
eckrin <bsh51515@naver.com>
2024-03-21 23:51:06 +0900

    240321: 15665

---
+++ src/B_2024_03/BOJ15665.java
@@ -0,0 +1,47 @@
+package B_2024_03;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ15665
+{
+    static int N, M;
+    static int[] arr;
+    static int[] rst;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line_1 = br.readLine().split(" ");
+        N = Integer.parseInt(line_1[0]);
+        M = Integer.parseInt(line_1[1]);
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(s -> Integer.parseInt(s)).toArray();
+        rst = new int[M];
+
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void dfs(int depth) throws IOException {
+        if(depth==M) {
+            for(int i : rst) {
+                bw.write(i+" ");
+            }
+            bw.write('\n');
+            return;
+        }
+
+        int past = 0; // 같은 레벨에서 두번 연속으로 dfs하지 않도록 방지
+        for(int i=0; i<N; i++) {
+            if(past!=arr[i]) {
+                rst[depth] = arr[i];
+                past = arr[i];
+                dfs(depth+1);
+                rst[depth] = 0;
+            }
+        }
+    }
+}
