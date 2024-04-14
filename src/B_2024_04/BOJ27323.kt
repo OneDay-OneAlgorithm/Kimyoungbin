4eaf7a0a265129152bbbd028f674f5db27b95ccd
eckrin <bsh51515@naver.com>
2024-04-15 01:52:20 +0900

    [Math] 27323

---
+++ src/B_2024_04/BOJ27323.kt
@@ -0,0 +1,13 @@
+package B_2024_04
+
+import java.io.*
+import java.util.*
+
+class BOJ27323 {
+    fun main() {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val A = br.readLine().toInt()
+        val B = br.readLine().toInt()
+        println(A*B)
+    }
+}
