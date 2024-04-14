eccbb848de9a4512358b66908b365f9451c1f439
eckrin <bsh51515@naver.com>
2024-04-15 01:52:26 +0900

    [Math] 15894

---
+++ src/B_2024_04/BOJ15894.kt
@@ -0,0 +1,12 @@
+package B_2024_04
+
+import java.io.*
+import java.util.*
+
+class BOJ15894 {
+    fun main() {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val n = br.readLine().toLong()
+        println(n*4)
+    }
+}
