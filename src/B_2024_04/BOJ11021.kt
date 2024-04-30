21f78eba19e274533d3fa9aa0825adf9c6080268
eckrin <bsh51515@naver.com>
2024-04-30 09:46:45 +0900

    [Math] 11021

---
+++ src/B_2024_04/BOJ11021.kt
@@ -0,0 +1,17 @@
+package B_2024_04
+
+import java.io.*
+import java.util.*
+
+class BOJ11021 {
+    fun main() {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val T = br.readLine().toInt();
+        for(i in 1..T) {
+            val line_1 = br.readLine().split(" ")
+            val A = line_1[0].toInt()
+            val B = line_1[1].toInt()
+            println("Case #${i}: ${A+B}")
+        }
+    }
+}
