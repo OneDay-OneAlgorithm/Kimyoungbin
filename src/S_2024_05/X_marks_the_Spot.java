aa30b534552edacc5ec396d90443f21435af8397
eckrin <bsh51515@naver.com>
2024-05-06 17:33:59 +0900

    [Impl-String] X marks the Spot

---
+++ src/S_2024_05/X_marks_the_Spot.java
@@ -0,0 +1,30 @@
+package S_2024_05;
+
+import java.io.*;
+import java.util.*;
+
+public class X_marks_the_Spot {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            String[] line_N = br.readLine().split(" ");
+            String s1 = line_N[0];
+            String s2 = line_N[1];
+            for(int j=0; j<s1.length(); j++) {
+                if(s1.charAt(j)=='x'||s1.charAt(j)=='X') {
+                    char target = s2.charAt(j);
+                    if('a'<=target && target<='z') {
+                        target += 'A'-'a';
+                    }
+                    sb.append(target);
+                    break;
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
