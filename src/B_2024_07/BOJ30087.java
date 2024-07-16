87e7f4767235aba1780a0d3019a04842589598de
eckrin <bsh51515@naver.com>
2024-07-16 19:31:05 +0900

    [HashMap] 30087

---
+++ src/B_2024_07/BOJ30087.java
@@ -0,0 +1,28 @@
+package B_2024_07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BOJ30087 {
+    static int N;
+    static Map<String, String> map = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map.put("Algorithm", "204");
+        map.put("DataAnalysis", "207");
+        map.put("ArtificialIntelligence", "302");
+        map.put("CyberSecurity", "B101");
+        map.put("Network", "303");
+        map.put("Startup", "501");
+        map.put("TestStrategy", "105");
+
+        for(int i=0; i<N; i++) {
+            System.out.println(map.get(br.readLine()));
+        }
+    }
+}
